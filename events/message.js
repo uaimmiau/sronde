9b118b1edcba0de75f1dd1b0d8af7fdc42cfeb3f
uaimmiau <zoltanchivay2137@gmail.com>
2020-10-06 22:53:15 +0200

    dupa

--- events/message.js
+++ events/message.js
@@ -1,38 +1,38 @@
 module.exports = (client, message) => {
-    // Ignore all bots
-    if (message.author.bot) return;
-    // Ignore DMs
-    if (!message.guild) return;
+  // Ignore all bots
+  if (message.author.bot) return;
+  // Ignore DMs
+  if (!message.guild) return;
 
-    const responseObject = require("../pressX/responseObject.json");
+  const responseObject = require("../pressX/responseObject.json");
 
-    //check if message starts with something which we can respond to in a hilarious way
-    for (var property in responseObject) {
-        if (message.content.toLowerCase().startsWith(property)) {
-            message.channel.send(responseObject[property]);
-        }
+  //check if message starts with something which we can respond to in a hilarious way
+  for (var property in responseObject) {
+    if (message.content.toLowerCase().startsWith(property)) {
+      message.channel.send(responseObject[property]);
     }
+  }
 
-    const sadge = client.emojis.find(emoji => emoji.name === "sadge");
-    // message.channel.send(`${poggies}`);
-    message.react(sadge.id)
+  const sadge = client.emojis.find((emoji) => emoji.name === "Sadge");
+  // message.channel.send(`${poggies}`);
+  message.react(sadge.id);
 
-    // Ignore messages not starting with the prefix (in config.json)
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+  // Ignore messages not starting with the prefix (in config.json)
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-    // Our standard argument/command name definition.
-    const args = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(/ +/g);
-    const command = args.shift().toLowerCase();
+  // Our standard argument/command name definition.
+  const args = message.content
+    .slice(client.config.prefix.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-    // Grab the command data from the client.commands Enmap
-    const cmd = client.commands.get(command);
+  // Grab the command data from the client.commands Enmap
+  const cmd = client.commands.get(command);
 
-    // If that command doesn't exist, silently exit and do nothing
-    if (!cmd) return message.reply("There's no such command!");
+  // If that command doesn't exist, silently exit and do nothing
+  if (!cmd) return message.reply("There's no such command!");
 
-    // Run the command
-    cmd.run(client, message, args);
+  // Run the command
+  cmd.run(client, message, args);
 };
