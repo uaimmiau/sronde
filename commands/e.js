8890cb0afb759844ec43b1d15fd2284e79516550
uaimmiau <zoltanchivay2137@gmail.com>
2020-10-14 12:37:15 +0200

    e4

--- commands/e.js
+++ commands/e.js
@@ -1,5 +1,7 @@
 exports.run = async (client, message, args) => {
+    if(args[0] == null) return message.reply("Must provide emoji name!");
     const e = client.emojis.find((emoji) => emoji.name == args[0]);
     if(e == null) return message.reply("There's no such emoji!");
     message.channel.send(`${e}`);
+    if(args[1] == "d") message.delete(0);
 }
